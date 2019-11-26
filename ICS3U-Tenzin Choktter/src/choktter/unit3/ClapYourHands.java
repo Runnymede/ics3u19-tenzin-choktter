b99ac067e83884088ab3f86441b5e5ae3c7da257
344116918 <344116918@C-3ZNGYR1.tdsb.on.ca>
2019-11-26 15:06:05 -0500

    TicTacToe program started

--- ICS3U-Tenzin Choktter/src/choktter/unit3/ClapYourHands.java
+++ ICS3U-Tenzin Choktter/src/choktter/unit3/ClapYourHands.java
@@ -9,6 +9,16 @@ package choktter.unit3;
 public class ClapYourHands {
 
 	public static void main(String[] args) {
+	
+		char [][] tttBoard = new char [3][3];
+		
+	
+		for (int row = 0; row < tttBoard.length; row++) {
+			for (int col = 0; col < tttBoard[0].length; col++) {
+				System.out.println(tttBoard[row][col]);
+			}
+			System.out.println();
+		}
 		
 
 	}
---
+++ ICS3U-Tenzin Choktter/src/choktter/unit3/TicTacToe.java
@@ -0,0 +1,15 @@
+package choktter.unit3;
+/**
+ * TicTacToe.java
+ * November 26, 2019
+ * This program is two people playing Tic-Tac-Toe 
+ * @author Tenzin Choktter
+ * <br>
+ */
+public class TicTacToe {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
