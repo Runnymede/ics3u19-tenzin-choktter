ae4ddc7881b519d03ffd9238da7ca587db33c554
344116918 <344116918@C-3ZNGYR1.tdsb.on.ca>
2019-12-03 13:44:08 -0500

    rolling dice almost done

--- ICS3U-Tenzin Choktter/src/choktter/unit3/Rolling2Dice.java
+++ ICS3U-Tenzin Choktter/src/choktter/unit3/Rolling2Dice.java
@@ -11,39 +11,84 @@ public class Rolling2Dice {
 	public static void main(String[] args) {
 
 
-		int [] dice2 = {1, 2, 3 , 4 , 5, 6}; 
 
-
+		int [] answer = {0,0,0,0,0,0,0,0,0,0,0};
 		int counter = 0; 
-		int answer1;
-	
-		
-		
+
+
+
+
 		do {
-			answer1 = (int) (Math.random() * 6) + 1;
+			int dice1 = (int) (Math.random() * 6);
+			int dice2= (int) (Math.random() * 6);
+			int total = dice1 + dice2;		
+
+
+			if (total == 2) {
+				answer [0] = answer [0] + 1;	
+			}
+
+			else if (total == 3) {
+				answer [1] = answer [1] + 1;
+			}
+			
+			else if (total == 4) {
+				answer [2] = answer [2] + 1;
+			}
+			else if (total == 5) {
+				answer [3] = answer [3] + 1;
+			}
+
+			else if (total == 6) {
+				answer [4] = answer [4] + 1;
+			}
+			else if (total == 7) {
+				answer [5] = answer [5] + 1;
+			}
+			else if (total == 8) {
+				answer [6] = answer [6] + 1;
+			}
+			else if (total == 9) {
+				answer [7] = answer [7] + 1;
+			}
+			else if (total == 10) {
+				answer [8] = answer [8] + 1;
+			}
+			else if (total == 11) {
+				answer [9] = answer [9] + 1;
+			}
+			else {
+				answer [10] = answer [10] + 1;
+			}
+
+
+
+
 
 			counter ++;
-		
-			int counter1 = 0;
+		}while (counter < 10000); 
 
-			int [] dice1 = {1, 2, 3 , 4 , 5, 6}; 
+		System.out.println(answer[0]);
+		System.out.println(answer[1]);
+		System.out.println(answer[2]);
+		System.out.println(answer[3]);
+		System.out.println(answer[4]);
+		System.out.println(answer[5]);
+		System.out.println(answer[6]);
+		System.out.println(answer[7]);
+		System.out.println(answer[8]);
+		System.out.println(answer[9]);
+		System.out.println(answer[10]);
+		
+		
 
-			if (answer1 == 1) {
-				counter1++;	
-			}
-				
-				
-				
-				System.out.println(counter1);
-		
-			
-		
-		}while (counter < 1000); 
-	
+
+
+
 
 
 
 	}
 
-	}
-	
+}
+
