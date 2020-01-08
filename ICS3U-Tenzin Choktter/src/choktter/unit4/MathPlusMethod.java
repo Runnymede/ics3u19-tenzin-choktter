ff2e9e707da8477f0c0e31b813f9b01a2017b476
344116918 <344116918@C-3ZNGYR1.tdsb.on.ca>
2020-01-08 15:05:46 -0500

    workin

--- ICS3U-Tenzin Choktter/src/choktter/unit4/MathPlusMethod.java
+++ ICS3U-Tenzin Choktter/src/choktter/unit4/MathPlusMethod.java
@@ -10,8 +10,11 @@ package choktter.unit4;
 
 public class MathPlusMethod {
 
+	
 	public static void main(String[] args) {
+
 		int a[] = new int [5];
+
 		a[0] = 6;
 		a[1] = 7;
 		a[2] = 3;
@@ -22,8 +25,17 @@ public class MathPlusMethod {
 		System.out.println(max(a));	
 		System.out.println(sum(a));
 		System.out.println(avrg(a));
+		System.out.println(min(a));
+		System.out.println(max(a));	
+		System.out.println(sum(a));
+		System.out.println(avrg(a));
 	}
 	//min
+	/**
+	 * this method returns the index of the first occurrance of the smallest number in the array
+	 * @param num - the array will be used 
+	 * @return
+	 */
 	public static int min (int num []) {
 		int min = num[0];
 		int index = 0;
@@ -71,52 +83,72 @@ public class MathPlusMethod {
 		return avrg;
 	}
 	//min 
-		public static int min (double num []) {
-			double min = num[0];
-			int index = 0;
+	public static int min (double num []) {
+		double min = num[0];
+		int index = 0;
 
-			for(int i = 1; i < num.length; i++) {
-				if (num[i] < min) {
-					min = num [1];
-					index = i; 
-				}	
-			}
-			return index; 
+		for(int i = 1; i < num.length; i++) {
+			if (num[i] < min) {
+				min = num [1];
+				index = i; 
+			}	
 		}
-		//max
-		public static int max (double num []) {
-			double max = num[0];
-			int index = 0;
+		return index; 
+	}
+	//max
+	public static int max (double num []) {
+		double max = num[0];
+		int index = 0;
 
-			for(int i = 1; i < num.length; i++) {
-				if (num[i] > max) {
-					max = num [1];
-					index = i; 
-				}	
-			}
-			return index; 
+		for(int i = 1; i < num.length; i++) {
+			if (num[i] > max) {
+				max = num [1];
+				index = i; 
+			}	
 		}
-		//sum
-		public static double  sum (double num []) {
-			double sum = num[0];
+		return index; 
+	}
+	//sum
+	public static double  sum (double num []) {
+		double sum = num[0];
 
-			for(int i = 1; i < num.length; i++) {
-				sum = num[0] + num[1] + num[2] + num[3] + num[4];	
-			}
-			return sum;
+		for(int i = 1; i < num.length; i++) {
+			sum = num[0] + num[1] + num[2] + num[3] + num[4];	
 		}
+		return sum;
+	}
 
-		//average 
-		public static double avrg (double num []) {
-			double sum = num[0];
-			double avrg; 
+	//average 
+	public static double avrg (double num []) {
+		double sum = num[0];
+		double avrg; 
 
-			for(int i = 1; i < num.length; i++) {
-				sum = num[0] + num[1] + num[2] + num[3] + num[4];		
-			}
-			avrg = sum/5;
-			return avrg;
+		for(int i = 1; i < num.length; i++) {
+			sum = num[0] + num[1] + num[2] + num[3] + num[4];		
 		}
+		avrg = sum/5;
+		return avrg;
+	}
+
+
+	//median 
+
+	public static double median (double num []) {
+		double sum = num[0];
+		double median = 0; 
+
+		
+			for(int i = 1; i < num.length; i++) {
+				if (num[i] < median) {
+					median = num [1];
+					sum = i; 	
+		}
+		median = sum/5;
+	}
+			return median;
+
+
+	}
 }
 
 
