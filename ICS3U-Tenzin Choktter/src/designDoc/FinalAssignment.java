d239beb559cd5da63f00d18b61b3810fe5b046ae
344116918 <344116918@C-3ZNGYR1.tdsb.on.ca>
2020-01-14 15:06:02 -0500

    ok

---
+++ ICS3U-Tenzin Choktter/src/designDoc/FinalAssignment.java
@@ -0,0 +1,29 @@
+package designDoc;
+/**
+ * Methods.java
+ * January 14, 2019
+ * This is my final assignment which is a G1 driving test 
+ * @author Tenzin Choktter
+ * <br>
+ */
+public class FinalAssignment {
+	public static void main(String[] args) {
+		String knowledgeQuestion [] = new String [10];
+	
+		
+		knowledgeQuestion [0] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [1] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [2] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [3] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [4] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [5] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [6] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [7] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [8] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		knowledgeQuestion [9] = "What does this road sign mean? 1, 2, 3, or 4?"; 
+		
+		String knowledgeAnswer [] = {"Merging traffic ahead", "No left turns", "Road branching off ahead", "Hidden intersection"};
+	
+	}
+
+}
